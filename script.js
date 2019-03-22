eb0ac3c15aa1a71d7ef0ba632fdef2c67812628d
Bull-Butcher <markjamesbutcher@gmail.com>
2019-03-22 17:17:20 +1100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById(".film").onmouseover = function() { mouseOver() };
+// document.getElementById(".film").onmouseout = function() { mouseOut() };
+
+// function mouseOver() {
+//     document.getElementById(".film").style.color = "white";
+// }
+
+// function mouseOut() {
+//     document.getElementById(".film").style.color = "black";
+// }
